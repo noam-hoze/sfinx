d45180cb8fb5b1f5ba4fd8b65185022bb8f2f8d7
noamhoze <noam.hoze@vayyar.com>
2025-07-06 10:33:48 -0400

    Cosmetic and textual changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,8 @@ import { Conversation } from "./components/conversation";
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-center p-12 bg-black text-white">
-            <h1 className="text-4xl font-bold mb-8 text-center">
-                Sfinx
-            </h1>
+            <h1 className="text-4xl font-bold mb-2 text-center">Vayyar</h1>
+            <h2 className="text-2xl text-center mb-8">Full-Stack Developer</h2>
             <Conversation />
         </main>
     );
