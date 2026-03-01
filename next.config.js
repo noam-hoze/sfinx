1d94e4669d4a849ae19eee9846db01767411e96f
noam-hoze <noam.hoze@gmail.com>
2026-02-28 21:29:09 -0500

    Configure app.sfinx.info for prod

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ const nextConfig = {
     typedRoutes: true,
     experimental: {
         serverActions: {
-            allowedOrigins: ["localhost:3000"],
+            allowedOrigins: ["localhost:3000", "app.sfinx.info"],
         },
     },
     images: {
